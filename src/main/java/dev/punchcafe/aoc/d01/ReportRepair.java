46fb96e29366a5717bdfdee08d6606e3a6127f0d
Luca Fruzza <lfruzza@hotmail.co.uk>
2020-12-01 18:46:41 +0000

    :recycle:

--- src/main/java/dev/punchcafe/aoc/d01/ReportRepair.java
+++ src/main/java/dev/punchcafe/aoc/d01/ReportRepair.java
@@ -8,13 +8,13 @@ public class ReportRepair {
     private int[] internalSortedArray;
 
     public ReportRepair(int[] problemSet) {
-        // If memory were no object, this could be solved with a boolean map of int value to whether it exists or not, for looking up.
         this.internalSortedArray = Arrays.stream(problemSet).sorted().toArray();
     }
 
     public Optional<Integer> calculateThreeBodyProblem(int targetNumber) {
         for (int i = 0; i < this.internalSortedArray.length; i++) {
-            // Another optimisation is to contract the array as you go through, as you know any passed elements wont be able to pair with anythign
+            // Another optimisation is to contract the array as you go through,
+            // as you know any passed elements won't be able to pair with anything
             int number = this.internalSortedArray[i];
             if (number > targetNumber) {
                 continue;
@@ -36,10 +36,10 @@ public class ReportRepair {
         return calculateTwoBodyProblem(this.internalSortedArray, targetNumber, 0, this.internalSortedArray.length - 1);
     }
 
-    public Optional<Integer> calculateTwoBodyProblem(int[] problemSet, int targetNumber, int startIndex, int endIndex) {
+    private Optional<Integer> calculateTwoBodyProblem(int[] sortedArray, int targetNumber, int startIndex, int endIndex) {
         for (int i = startIndex; i <= endIndex; i++) {
             // Another optimisation is to contract the array as you go through, as you know any passed elements wont be able to pair with anythign
-            int number = problemSet[i];
+            int number = sortedArray[i];
             if (number > targetNumber) {
                 continue;
             }
@@ -47,27 +47,25 @@ public class ReportRepair {
             // We can slice the array by starting from i, since we know all up to that number have been checked,
             // meaning by symmetry we can rule them out of being suitable pairs for future numbers, thus removing the need
             // to include them in is number present
-            if (isNumberPresent(targetPair, problemSet, i, problemSet.length - 1)) {
+            if (isNumberPresent(targetPair, sortedArray, i, sortedArray.length - 1)) {
                 return Optional.of(number * targetPair);
             }
-            ;
         }
         return Optional.empty();
     }
 
-    private boolean isNumberPresent(int number, final int[] sortedProblemSet, int beginIndex, int endIndex) {
+    private boolean isNumberPresent(int number, final int[] sortedArray, int beginIndex, int endIndex) {
+        // If memory is no issue, this could be solved with a Map<Integer, Boolean> of int value to whether it exists or not.
         if (beginIndex == endIndex) {
-            return sortedProblemSet[beginIndex] == number;
+            return sortedArray[beginIndex] == number;
         }
         final var leftHandSideIndex = (beginIndex + endIndex) / 2;
         final var rightHandSideIndex = leftHandSideIndex + 1;
-        final var isLeftHandSide = number <= sortedProblemSet[leftHandSideIndex];
+        final var isLeftHandSide = number <= sortedArray[leftHandSideIndex];
         if (isLeftHandSide) {
-            return isNumberPresent(number, sortedProblemSet, beginIndex, leftHandSideIndex);
+            return isNumberPresent(number, sortedArray, beginIndex, leftHandSideIndex);
         } else {
-            return isNumberPresent(number, sortedProblemSet, rightHandSideIndex, endIndex);
+            return isNumberPresent(number, sortedArray, rightHandSideIndex, endIndex);
         }
     }
-
-    ;
 }
