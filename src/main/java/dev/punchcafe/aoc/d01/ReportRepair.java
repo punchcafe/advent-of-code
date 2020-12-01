53399dc31aae733a23a16dff6f62027baa4deadc
Luca Fruzza <lfruzza@hotmail.co.uk>
2020-12-01 18:42:28 +0000

    :christmas_tree: add day 1 challenge 2 solution

--- src/main/java/dev/punchcafe/aoc/d01/ReportRepair.java
+++ src/main/java/dev/punchcafe/aoc/d01/ReportRepair.java
@@ -5,44 +5,69 @@ import java.util.Optional;
 
 public class ReportRepair {
 
-    private int[] sortedArray;
-    private int targetNumber;
+    private int[] internalSortedArray;
 
-    public ReportRepair(int[] problemSet, int targetNumber){
+    public ReportRepair(int[] problemSet) {
         // If memory were no object, this could be solved with a boolean map of int value to whether it exists or not, for looking up.
-        this.sortedArray = Arrays.stream(problemSet).sorted().toArray();
-        this.targetNumber = targetNumber;
+        this.internalSortedArray = Arrays.stream(problemSet).sorted().toArray();
     }
 
-    public Optional<Integer> calculate(){
-        for(int i = 0; i < sortedArray.length; i++){
+    public Optional<Integer> calculateThreeBodyProblem(int targetNumber) {
+        for (int i = 0; i < this.internalSortedArray.length; i++) {
             // Another optimisation is to contract the array as you go through, as you know any passed elements wont be able to pair with anythign
-            int number = sortedArray[i];
-            if(number > targetNumber){
+            int number = this.internalSortedArray[i];
+            if (number > targetNumber) {
+                continue;
+            }
+            int totalRemainder = targetNumber - number;
+
+            final var entityResult = this.calculateTwoBodyProblem(this.internalSortedArray,
+                                                                  totalRemainder,
+                                                                  i,
+                                                                  this.internalSortedArray.length - 1);
+            if (entityResult.isPresent()) {
+                return Optional.of(entityResult.get() * number);
+            }
+        }
+        return Optional.empty();
+    }
+
+    public Optional<Integer> calculateTwoBodyProblem(int targetNumber) {
+        return calculateTwoBodyProblem(this.internalSortedArray, targetNumber, 0, this.internalSortedArray.length - 1);
+    }
+
+    public Optional<Integer> calculateTwoBodyProblem(int[] problemSet, int targetNumber, int startIndex, int endIndex) {
+        for (int i = startIndex; i <= endIndex; i++) {
+            // Another optimisation is to contract the array as you go through, as you know any passed elements wont be able to pair with anythign
+            int number = problemSet[i];
+            if (number > targetNumber) {
                 continue;
             }
             int targetPair = targetNumber - number;
             // We can slice the array by starting from i, since we know all up to that number have been checked,
             // meaning by symmetry we can rule them out of being suitable pairs for future numbers, thus removing the need
             // to include them in is number present
-            if(isNumberPresent(targetPair, i, sortedArray.length - 1)){
+            if (isNumberPresent(targetPair, problemSet, i, problemSet.length - 1)) {
                 return Optional.of(number * targetPair);
-            };
+            }
+            ;
         }
         return Optional.empty();
     }
 
-    private boolean isNumberPresent(int number, int beginIndex, int endIndex){
-        if(beginIndex == endIndex){
-            return sortedArray[beginIndex] == number;
+    private boolean isNumberPresent(int number, final int[] sortedProblemSet, int beginIndex, int endIndex) {
+        if (beginIndex == endIndex) {
+            return sortedProblemSet[beginIndex] == number;
         }
         final var leftHandSideIndex = (beginIndex + endIndex) / 2;
         final var rightHandSideIndex = leftHandSideIndex + 1;
-        final var isLeftHandSide = number <= sortedArray[leftHandSideIndex];
-        if(isLeftHandSide){
-            return isNumberPresent(number, beginIndex, leftHandSideIndex);
+        final var isLeftHandSide = number <= sortedProblemSet[leftHandSideIndex];
+        if (isLeftHandSide) {
+            return isNumberPresent(number, sortedProblemSet, beginIndex, leftHandSideIndex);
         } else {
-            return isNumberPresent(number, rightHandSideIndex, endIndex);
+            return isNumberPresent(number, sortedProblemSet, rightHandSideIndex, endIndex);
         }
-    };
+    }
+
+    ;
 }
--- src/test/java/dev/punchcafe/aoc/d01/ReportRepairTest.java
+++ src/test/java/dev/punchcafe/aoc/d01/ReportRepairTest.java
@@ -1,5 +1,8 @@
 package dev.punchcafe.aoc.d01;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.io.File;
 import java.io.IOException;
 
@@ -13,20 +16,38 @@ public class ReportRepairTest {
     private ReportRepair reportRepair;
 
     @Test
-    void shouldWork() {
-        reportRepair = new ReportRepair(new int[] {1, 2, 3, 4, 5}, 9);
-        final var result = reportRepair.calculate();
-        System.out.println(result);
+    void shouldReturnEmptyIfNoSolutionToTwoBodyProblem() {
+        reportRepair = new ReportRepair(new int[] {1, 2, 3, 4, 5});
+        final var result = reportRepair.calculateTwoBodyProblem(30);
+        assertTrue(result.isEmpty());
     }
 
     @Test
-    void getSolution() throws IOException {
+    void shouldReturnEmptyIfNoSolutionToThreeBodyProblem() {
+        reportRepair = new ReportRepair(new int[] {1, 2, 3, 4, 5});
+        final var result = reportRepair.calculateThreeBodyProblem(30);
+        assertTrue(result.isEmpty());
+    }
+
+    @Test
+    void getSolutionTwoBodySolution() throws IOException {
         String userDirectory = new File("").getAbsolutePath();
         final var problemSetArray = Files.lines(Path.of(userDirectory + "/src/test/resources/d01a/input.txt"))
                 .mapToInt(Integer::parseInt)
                 .toArray();
-        reportRepair = new ReportRepair(problemSetArray, 2020);
-        final var result = reportRepair.calculate();
-        System.out.println(result);
+        reportRepair = new ReportRepair(problemSetArray);
+        final var result = reportRepair.calculateTwoBodyProblem(2020);
+        assertEquals(result.get(), 319531);
+    }
+
+    @Test
+    void getSolutionThreeBodySolution() throws IOException {
+        String userDirectory = new File("").getAbsolutePath();
+        final var problemSetArray = Files.lines(Path.of(userDirectory + "/src/test/resources/d01a/input.txt"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        reportRepair = new ReportRepair(problemSetArray);
+        final var result = reportRepair.calculateThreeBodyProblem(2020);
+        assertEquals(result.get(), 244300320);
     }
 }
